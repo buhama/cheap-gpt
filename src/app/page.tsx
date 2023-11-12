39d96ec81323c51331438367e3f8e615a7573b9d
buhama <hassan@hassanahmed.ca>
2023-11-11 19:53:50 -0500

    remove please

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import Chat from '@/components/chat';
 export default async function Home() {
 	return (
 		<main className='w-screen flex justify-center h-screen items-end'>
-			<p>PLEASE</p>
 			<Chat />
 		</main>
 	);
