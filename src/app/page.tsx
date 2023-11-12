03d3bb87baba630157cfc46d7cd58dd88785e7a3
buhama <hassan@hassanahmed.ca>
2023-11-11 20:56:10 -0500

    formattin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Chat from '@/components/chat';
 
 export default async function Home() {
 	return (
-		<main className='w-screen flex justify-center h-[100vh] overflow-auto items-end'>
+		<main className='w-screen flex justify-center h-full items-end'>
 			<Chat />
 		</main>
 	);
--- src/components/chat.tsx
+++ src/components/chat.tsx
@@ -1,10 +1,11 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Input } from './ui/input';
 import { Button } from './ui/button';
 import { Skeleton } from './ui/skeleton';
 import Markdown from 'react-markdown';
+import { Textarea } from './ui/textarea';
 
 interface Message {
 	type: 'user' | 'bot';
@@ -16,6 +17,14 @@ const Chat = () => {
 	const [currentMessage, setCurrentMessage] = useState<string>('');
 	const [currentResponse, setCurrentResponse] = useState<string>('');
 	const [loading, setLoading] = useState(false);
+
+	const containerRef = useRef<HTMLDivElement | null>(null);
+
+	useEffect(() => {
+		if (containerRef.current) {
+			containerRef.current.scrollTop = containerRef.current.scrollHeight;
+		}
+	}, [messages, currentResponse]);
 
 	const submit = async (e: React.FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
@@ -68,7 +77,10 @@ const Chat = () => {
 	return (
 		<div className='max-w-4xl w-full mb-20 overflow-auto max-h-fit'>
 			<div className='flex flex-col bg-white dark:bg-zinc-900'>
-				<div className='flex flex-col h-full justify-end overflow-y-auto p-4 space-y-4'>
+				<div
+					className='flex flex-col overflow-y-auto p-4 space-y-4 h-[calc(100vh_-_192px)]'
+					ref={containerRef}
+				>
 					{messages.map(({ type, message }, i) => (
 						<div className='flex items-end space-x-4' key={i}>
 							<div
@@ -80,11 +92,10 @@ const Chat = () => {
 								<div className='font-medium'>
 									{type === 'bot' ? 'Bot' : 'You'}
 								</div>
-								{type === 'bot' ? (
-									<Markdown className='mt-1'>{message}</Markdown>
-								) : (
-									<div className='mt-1'>{message}</div>
-								)}
+
+								<Markdown className='mt-1 flex flex-col gap-y-4'>
+									{message}
+								</Markdown>
 							</div>
 						</div>
 					))}
@@ -93,7 +104,10 @@ const Chat = () => {
 							<div className='w-10 h-10 rounded-full bg-slate-900'></div>
 							<div className='flex flex-col w-fit'>
 								<div className='font-medium'>Bot</div>
-								<Markdown className='mt-1'>{currentResponse}</Markdown>
+
+								<Markdown className='mt-1 flex flex-col gap-y-4'>
+									{currentResponse}
+								</Markdown>
 							</div>
 						</div>
 					)}
@@ -113,7 +127,7 @@ const Chat = () => {
 					className='flex items-center p-4 border-t dark:border-zinc-700'
 					onSubmit={(e) => submit(e)}
 				>
-					<Input
+					<Textarea
 						value={currentMessage}
 						onChange={(e) => setCurrentMessage(e.target.value)}
 						name='message'
