fe310d742fd605ae2c4a1e9db347516a4545b949
buhama <hassan@hassanahmed.ca>
2023-11-11 19:49:14 -0500

    please

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,10 @@
 import Chat from '@/components/chat';
-import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
-import { OpenAIStream } from 'ai';
-import Image from 'next/image';
-import OpenAI from 'openai';
+
 
 export default async function Home() {
 	return (
 		<main className='w-screen flex justify-center h-screen items-end'>
+			<p>PLEASE</p>
 			<Chat />
 		</main>
 	);
